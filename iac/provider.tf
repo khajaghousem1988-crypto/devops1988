db29905855ca551b296a3a9cbbee2784254d2359
khajaghousem1988-crypto <khajaghousem1988@gmail.com>
2026-09-01 19:38:36 +0530

    Fixing Provider Block

--- iac/provider.tf
+++ iac/provider.tf
@@ -1,7 +1,6 @@
 provider "aws" {
-  region  = var.aws_region
+  region = var.aws_region
   # profile = "devops1988"
-  # profile {
   default_tags {
     tags = {
       Project     = var.project_name
